c277344abf134a5a57fb908fe626b4cbf3c1a36a
akashdeep023 <ad3500476@gmail.com>
2024-11-11 03:06:52 +0530

    Page Not Found Update

--- frontend/src/pages/PageNotFound.jsx
+++ frontend/src/pages/PageNotFound.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import "../css/Page_Not_Found.css";
+import { Link } from "react-router-dom";
 const PageNotFound = () => {
-	return <div>PageNotFound</div>;
+	return (
+		<div className="page_not_found">
+			<h2>Page Not Found</h2>
+			<h1>404</h1>
+			<Link to={"/"} className="page_not_found_link">
+				Back
+			</Link>
+		</div>
+	);
 };
 
 export default PageNotFound;
