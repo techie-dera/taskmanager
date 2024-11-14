5ce6dc50da7f067250bf3ccaa1e9f6adb39f87b3
akashdeep023 <ad3500476@gmail.com>
2024-11-14 23:56:30 +0530

    Update UI

--- frontend/src/pages/Backlog.jsx
+++ frontend/src/pages/Backlog.jsx
@@ -1,31 +1,55 @@
 import "../css/Dashboard.css";
-import "../css/Backlog.css"
+import "../css/Backlog.css";
 
 const Backlog = () => {
-  return (
-    <div className="backlog-container">
-      <div className="backlog-box">
-        <h3>High Probability</h3>
-        <p>Tasks with a high chance of being prioritized.</p>
-      </div>
-      <div className="backlog-box">
-        <h3>Moderate Probability</h3>
-        <p>Tasks with a moderate chance of being prioritized.</p>
-      </div>
-      <div className="backlog-box">
-        <h3>Low Probability</h3>
-        <p>Tasks with a lower chance of being prioritized.</p>
-      </div>
-      <div className="backlog-box">
-        <h3>Low Probability</h3>
-        <p>Tasks with a lower chance of being prioritized.</p>
-      </div>
-      <div className="backlog-box">
-        <h3>Low Probability</h3>
-        <p>Tasks with a lower chance of being prioritized.</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className="backlog-container">
+			<div className="backlog-box">
+				<h3>High Probability</h3>
+				<p>Tasks with a high chance of being prioritized.</p>
+			</div>
+			<div className="backlog-box">
+				<h3>Moderate Probability</h3>
+				<p>Tasks with a moderate chance of being prioritized.</p>
+			</div>
+			<div className="backlog-box">
+				<h3>Low Probability</h3>
+				<p>Tasks with a lower chance of being prioritized.</p>
+			</div>
+			<div className="backlog-box">
+				<h3>Low Probability</h3>
+				<p>Tasks with a lower chance of being prioritized.</p>
+			</div>
+			<div className="backlog-box">
+				<h3>Low Probability</h3>
+				<p>Tasks with a lower chance of being prioritized.</p>
+			</div>
+			<div className="backlog-box">
+				<h3>Low Probability</h3>
+				<p>Tasks with a lower chance of being prioritized.</p>
+			</div>
+			<div className="backlog-box">
+				<h3>Low Probability</h3>
+				<p>Tasks with a lower chance of being prioritized.</p>
+			</div>
+			<div className="backlog-box">
+				<h3>Low Probability</h3>
+				<p>Tasks with a lower chance of being prioritized.</p>
+			</div>
+			<div className="backlog-box">
+				<h3>Low Probability</h3>
+				<p>Tasks with a lower chance of being prioritized.</p>
+			</div>
+			<div className="backlog-box">
+				<h3>Low Probability</h3>
+				<p>Tasks with a lower chance of being prioritized.</p>
+			</div>
+			<div className="backlog-box">
+				<h3>Low Probability</h3>
+				<p>Tasks with a lower chance of being prioritized.</p>
+			</div>
+		</div>
+	);
 };
 
 export default Backlog;
