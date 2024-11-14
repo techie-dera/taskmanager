2667c71a9276f8375b605ec98b8297d3e786338b
akashdeep023 <ad3500476@gmail.com>
2024-11-15 03:44:22 +0530

    Update Backlog

--- frontend/src/pages/Backlog.jsx
+++ frontend/src/pages/Backlog.jsx
@@ -1,52 +1,45 @@
 import "../css/Dashboard.css";
 import "../css/Backlog.css";
+import { TbDots } from "react-icons/tb";
 
 const Backlog = () => {
 	return (
 		<div className="backlog-container">
 			<div className="backlog-box">
+				<div className="priority-menu">
+					<div>
+						<div className="circle-box"></div>
+						<p>HIGH PRIORITY</p>
+						<div className="circle-name-box">AK</div>
+					</div>
+					<TbDots fontSize={18} />
+				</div>
 				<h3>High Probability</h3>
 				<p>Tasks with a high chance of being prioritized.</p>
 			</div>
 			<div className="backlog-box">
-				<h3>Moderate Probability</h3>
-				<p>Tasks with a moderate chance of being prioritized.</p>
+				<div className="priority-menu">
+					<div>
+						<div className="circle-box"></div>
+						<p>HIGH PRIORITY</p>
+						<div className="circle-name-box">AK</div>
+					</div>
+					<TbDots fontSize={18} />
+				</div>
+				<h3>High Probability</h3>
+				<p>Tasks with a high chance of being prioritized.</p>
 			</div>
 			<div className="backlog-box">
-				<h3>Low Probability</h3>
-				<p>Tasks with a lower chance of being prioritized.</p>
-			</div>
-			<div className="backlog-box">
-				<h3>Low Probability</h3>
-				<p>Tasks with a lower chance of being prioritized.</p>
-			</div>
-			<div className="backlog-box">
-				<h3>Low Probability</h3>
-				<p>Tasks with a lower chance of being prioritized.</p>
-			</div>
-			<div className="backlog-box">
-				<h3>Low Probability</h3>
-				<p>Tasks with a lower chance of being prioritized.</p>
-			</div>
-			<div className="backlog-box">
-				<h3>Low Probability</h3>
-				<p>Tasks with a lower chance of being prioritized.</p>
-			</div>
-			<div className="backlog-box">
-				<h3>Low Probability</h3>
-				<p>Tasks with a lower chance of being prioritized.</p>
-			</div>
-			<div className="backlog-box">
-				<h3>Low Probability</h3>
-				<p>Tasks with a lower chance of being prioritized.</p>
-			</div>
-			<div className="backlog-box">
-				<h3>Low Probability</h3>
-				<p>Tasks with a lower chance of being prioritized.</p>
-			</div>
-			<div className="backlog-box">
-				<h3>Low Probability</h3>
-				<p>Tasks with a lower chance of being prioritized.</p>
+				<div className="priority-menu">
+					<div>
+						<div className="circle-box"></div>
+						<p>HIGH PRIORITY</p>
+						<div className="circle-name-box">AK</div>
+					</div>
+					<TbDots fontSize={18} />
+				</div>
+				<h3>High Probability</h3>
+				<p>Tasks with a high chance of being prioritized.</p>
 			</div>
 		</div>
 	);
