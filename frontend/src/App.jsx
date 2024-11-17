dab3ec571f9e0f7e4d81b56224e15e4dc1a9d00a
akashdeep023 <ad3500476@gmail.com>
2024-11-17 17:20:12 +0530

    Modify Code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,56 +1,71 @@
-import React, { Suspense } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import { Provider } from "react-redux";
+import React, { Suspense, useEffect } from "react";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import PageNotFound from "./pages/PageNotFound";
-import store from "./redux/store";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import Register_Login from "./pages/Register_Login";
 import PrivateRoute from "./components/PrivateRoute";
+import Loading from "./components/Loading";
+import { useDispatch, useSelector } from "react-redux";
+import { addAuth } from "./redux/slices/authSlice";
+import { setLoading } from "./redux/slices/stateSlice";
 
 function App() {
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const { pathname } = useLocation();
+	const loading = useSelector((store) => store.state.loading);
+	const token = localStorage.getItem("token");
+	const getAuthUser = (token) => {
+		dispatch(setLoading(true));
+		fetch(`${import.meta.env.VITE_BACKEND_URL}/api/user/profile`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${token}`,
+			},
+		})
+			.then((res) => res.json())
+			.then((json) => {
+				dispatch(setLoading(false));
+				if (json?.data) {
+					dispatch(addAuth(json.data));
+					navigate("/");
+				} else {
+					navigate("/login");
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+				dispatch(setLoading(false));
+			});
+	};
+
+	useEffect(() => {
+		if (token) {
+			getAuthUser(token);
+		} else {
+			navigate("/login");
+		}
+	}, [token, pathname]);
+
 	return (
-		<Provider store={store}>
-			<BrowserRouter>
-				{/* <Header /> */}
-				<ToastContainer
-					position={"top-right"}
-					autoClose={3000}
-					hideProgressBar={false}
-					newestOnTop={false}
-					closeOnClick
-					rtl={false}
-					pauseOnFocusLoss
-					draggable
-					pauseOnHover
-					theme="light"
-					stacked
-					limit={3}
-					toastStyle={{
-						border: "1px solid #dadadaaa",
-						textTransform: "capitalize",
-					}}
-				/>
-				<Suspense fallback={<div>loading...</div>}>
-					<Routes>
-						<Route
-							path="/"
-							element={
-								<PrivateRoute>
-									<Dashboard />
-								</PrivateRoute>
-							}
-						/>
-						<Route path="/login" element={<Register_Login />} />
-						<Route path="*" element={<PageNotFound />} />
-					</Routes>
-				</Suspense>
-				{/* <Footer /> */}
-			</BrowserRouter>
-		</Provider>
+		<div>
+			{loading && <Loading />}
+			<Suspense fallback={<Loading />}>
+				<Routes>
+					<Route
+						path="/"
+						element={
+							<PrivateRoute>
+								<Dashboard />
+							</PrivateRoute>
+						}
+					/>
+					<Route path="/login" element={<Register_Login />} />
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
+			</Suspense>
+		</div>
 	);
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<ToastContainer
+					position={"top-right"}
+					autoClose={3000}
+					hideProgressBar={false}
+					newestOnTop={false}
+					closeOnClick
+					rtl={false}
+					pauseOnFocusLoss
+					draggable
+					pauseOnHover
+					theme="light"
+					stacked
+					limit={3}
+					toastStyle={{
+						border: "1px solid #dadadaaa",
+						textTransform: "capitalize",
+					}}
+				/>
+				<App />
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>
 );
