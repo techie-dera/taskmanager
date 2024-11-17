3de16e4d5969ed2c8fae14d5d1405ba139abc262
akashdeep023 <ad3500476@gmail.com>
2024-11-18 04:54:57 +0530

    Share Feature

--- frontend/src/components/PopUp.jsx
+++ frontend/src/components/PopUp.jsx
@@ -2,16 +2,32 @@ import React from "react";
 import "../css/PopUp.css";
 import { useDispatch } from "react-redux";
 import { setTaskDeleteM } from "../redux/slices/stateSlice";
+import { toast } from "react-toastify";
+
 export const TaskMenu = ({ setTaskMenuP }) => {
 	const dispatch = useDispatch();
 	const handleTaskDelete = () => {
 		dispatch(setTaskDeleteM(true));
 		setTaskMenuP(false);
 	};
+	const handleSharelink = (id) => {
+		if (!navigator.clipboard) {
+			toast.error(
+				"Your browser does not support copying text to the clipboard."
+			);
+			return;
+		}
+		navigator.clipboard.writeText(
+			`${import.meta.env.VITE_FRONTEND_URL}/task/${id}`
+		);
+
+		setTaskMenuP(false);
+		toast.success("Link Copied");
+	};
 	return (
 		<div className="popup-box">
 			<button>Edit</button>
-			<button>Share</button>
+			<button onClick={() => handleSharelink("id-ksmx")}>Share</button>
 			<button className="popup-delete" onClick={handleTaskDelete}>
 				Delete
 			</button>
