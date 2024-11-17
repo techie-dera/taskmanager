e4473a5daf3972e7482f9d634aae1e2360ef2602
akashdeep023 <ad3500476@gmail.com>
2024-11-17 17:19:32 +0530

    Create Loading && Delete components

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../css/Loading.css";
+
+const Loading = () => {
+	return (
+		<div className="loading-container">
+			<div className="loading-box">
+				<div className="loading"></div>
+			</div>
+		</div>
+	);
+};
+
+export default Loading;
