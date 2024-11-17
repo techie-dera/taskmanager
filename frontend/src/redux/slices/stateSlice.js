5bfafda49c31af5dfb6f809356831d94c0a292af
akashdeep023 <ad3500476@gmail.com>
2024-11-17 21:31:53 +0530

    Create Model state slice

--- frontend/src/redux/slices/stateSlice.js
+++ frontend/src/redux/slices/stateSlice.js
@@ -5,6 +5,11 @@ const stateSlice = createSlice({
 	initialState: {
 		dashboardSection: "board",
 		loading: false,
+		addPeopleM: false,
+		addedPeopleM: true,
+		logoutM: false,
+		taskDeleteM: false,
+		taskCardM: false,
 	},
 	reducers: {
 		updateDashboardSection: (state, action) => {
@@ -13,7 +18,30 @@ const stateSlice = createSlice({
 		setLoading: (state, action) => {
 			state.loading = action.payload;
 		},
+		setAddPeopleM: (state, action) => {
+			state.addPeopleM = action.payload;
+		},
+		setAddedPeopleM: (state, action) => {
+			state.addedPeopleM = action.payload;
+		},
+		setLogoutM: (state, action) => {
+			state.logoutM = action.payload;
+		},
+		setTaskDeleteM: (state, action) => {
+			state.taskDeleteM = action.payload;
+		},
+		setTaskCardM: (state, action) => {
+			state.taskCardM = action.payload;
+		},
 	},
 });
-export const { updateDashboardSection, setLoading } = stateSlice.actions;
+export const {
+	updateDashboardSection,
+	setLoading,
+	setAddPeopleM,
+	setAddedPeopleM,
+	setLogoutM,
+	setTaskDeleteM,
+	setTaskCardM,
+} = stateSlice.actions;
 export default stateSlice.reducer;
