253acb1189d63d73bc242a92090eb6ed747b8596
akashdeep023 <ad3500476@gmail.com>
2024-11-19 05:01:09 +0530

    Create Task model

---
+++ backend/models/task.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Checklist = require("./checklist");
+
+const taskSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+			unique: true,
+			trim: true,
+		},
+		priority: {
+			type: String,
+			required: true,
+		},
+		category: {
+			type: String,
+			required: true,
+			default: "to-do",
+		},
+		checklist: [
+			{
+				name: {
+					type: String,
+					trim: true,
+					required: true,
+				},
+				isDone: {
+					type: Boolean,
+					default: false,
+					required: true,
+				},
+			},
+		],
+		dueDate: Date,
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const Task = mongoose.model("Task", taskSchema);
+module.exports = Task;
