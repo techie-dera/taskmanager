c5968452949a02beac86c8cf8a10141e3f55443e
akashdeep023 <ad3500476@gmail.com>
2024-11-19 04:57:22 +0530

    Add to store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./slices/authSlice";
 import stateSlice from "./slices/stateSlice";
+import taskSlice from "./slices/taskSlice";
 
 const store = configureStore({
 	reducer: {
 		auth: authSlice,
 		state: stateSlice,
+		task: taskSlice,
 	},
 });
 
