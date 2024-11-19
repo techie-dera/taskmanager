693e4279df2664afacc1a833e6362a96a3cec1d2
akashdeep023 <ad3500476@gmail.com>
2024-11-20 03:53:31 +0530

    Create useUpdateTask hook

---
+++ frontend/src/hooks/useUpdateTask.js
@@ -0,0 +1,48 @@
+import getHeader from "../utils/header";
+import { toast } from "react-toastify";
+import { setTaskCardM, setTaskM } from "../redux/slices/stateSlice";
+import { updateTodoTask } from "../redux/slices/taskSlice";
+const useUpdateTask = (
+	e,
+	setLoad,
+	title,
+	priority,
+	checklist,
+	dueDate,
+	dispatch,
+	id
+) => {
+	setLoad("Loading...");
+	e.target.disabled = true;
+	fetch(`${import.meta.env.VITE_BACKEND_URL}/api/task/${id}`, {
+		method: "PUT",
+		headers: getHeader(),
+		body: JSON.stringify({
+			title: title,
+			priority: priority,
+			checklist: checklist,
+			dueDate: dueDate,
+		}),
+	})
+		.then((response) => response.json())
+		.then((json) => {
+			setLoad("");
+			e.target.disabled = false;
+			if (json?.message === "success") {
+				toast.success("Task Added Successfully");
+				dispatch(updateTodoTask(json.data));
+				dispatch(setTaskCardM(false));
+				dispatch(setTaskM(""));
+			} else {
+				toast.error(json?.message);
+			}
+		})
+		.catch((error) => {
+			console.error("Error:", error);
+			setLoad("");
+			toast.error("Something went wrong");
+			e.target.disabled = false;
+		});
+};
+
+export default useUpdateTask;
