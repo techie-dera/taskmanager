6b180e9b0b55874ff6b89cc36f379d9e0e2a1ac5
akashdeep023 <ad3500476@gmail.com>
2024-11-19 05:34:05 +0530

    Update AllTask hook

--- frontend/src/hooks/useAllTask.js
+++ frontend/src/hooks/useAllTask.js
@@ -2,6 +2,12 @@ import { useDispatch } from "react-redux";
 import getHeader from "../utils/header";
 import { toast } from "react-toastify";
 import { useEffect } from "react";
+import {
+	setBacklog,
+	setTodo,
+	setInProgress,
+	setDone,
+} from "../redux/slices/taskSlice";
 const useAllTask = () => {
 	const dispatch = useDispatch();
 	const fetchData = () => {
@@ -12,14 +18,16 @@ const useAllTask = () => {
 			.then((response) => response.json())
 			.then((json) => {
 				if (json?.message === "success") {
-					console.log(json.data);
+					dispatch(setBacklog(json.data.backlog));
+					dispatch(setTodo(json.data.todo));
+					dispatch(setInProgress(json.data.inProgress));
+					dispatch(setDone(json.data.done));
 				} else {
-					toast.error(json?.message);
+					console.log(json?.message);
 				}
 			})
 			.catch((error) => {
 				console.error("Error:", error);
-				toast.error("Something went wrong");
 			});
 	};
 
