f157379355edad7abbb36fdc2876f634fe290ca1
akashdeep023 <ad3500476@gmail.com>
2024-11-21 02:01:41 +0530

    Update analytics controller

--- backend/controllers/analytics.js
+++ backend/controllers/analytics.js
@@ -1,21 +1,83 @@
 const Task = require("../models/task");
 
 const getAnalytics = async (req, res) => {
-	let backlog = await Task.find({ category: "backlog" }).countDocuments();
-	let todo = await Task.find({ category: "to-do" }).countDocuments();
-	let inProgress = await Task.find({
-		category: "in-progress",
+	let backlog = await Task.find({
+		$or: [
+			{ $and: [{ userName: req.user._id }, { category: "backlog" }] },
+			{ $and: [{ assign: req.user.email }, { category: "backlog" }] },
+		],
 	}).countDocuments();
-	let done = await Task.find({ category: "done" }).countDocuments();
-	let high = await Task.find({ priority: "High Priority" }).countDocuments();
+	let todo = await Task.find({
+		$or: [
+			{ $and: [{ userName: req.user._id }, { category: "to-do" }] },
+			{ $and: [{ assign: req.user.email }, { category: "to-do" }] },
+		],
+	}).countDocuments();
+	let inProgress = await Task.find({
+		$or: [
+			{ $and: [{ userName: req.user._id }, { category: "in-progress" }] },
+			{ $and: [{ assign: req.user.email }, { category: "in-progress" }] },
+		],
+	}).countDocuments();
+	let done = await Task.find({
+		$or: [
+			{ $and: [{ userName: req.user._id }, { category: "done" }] },
+			{ $and: [{ assign: req.user.email }, { category: "done" }] },
+		],
+	}).countDocuments();
+	let high = await Task.find({
+		$or: [
+			{
+				$and: [
+					{ userName: req.user._id },
+					{ priority: "High Priority" },
+				],
+			},
+			{
+				$and: [
+					{ assign: req.user.email },
+					{ priority: "High Priority" },
+				],
+			},
+		],
+	}).countDocuments();
 	let moderate = await Task.find({
-		priority: "Moderate Priority",
+		$or: [
+			{
+				$and: [
+					{ userName: req.user._id },
+					{ priority: "Moderate Priority" },
+				],
+			},
+			{
+				$and: [
+					{ assign: req.user.email },
+					{ priority: "Moderate Priority" },
+				],
+			},
+		],
 	}).countDocuments();
 	let low = await Task.find({
-		priority: "Low Priority",
+		$or: [
+			{
+				$and: [
+					{ userName: req.user._id },
+					{ priority: "Low Priority" },
+				],
+			},
+			{
+				$and: [
+					{ assign: req.user.email },
+					{ priority: "Low Priority" },
+				],
+			},
+		],
 	}).countDocuments();
 	let dueDate = await Task.find({
-		dueDate: { $ne: "" },
+		$or: [
+			{ $and: [{ userName: req.user._id }, { dueDate: { $ne: "" } }] },
+			{ $and: [{ assign: req.user.email }, { dueDate: { $ne: "" } }] },
+		],
 	}).countDocuments();
 	res.status(200).send({
 		message: "success",
