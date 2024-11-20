020063a8fb2ba056dbc5a7320e964cd695ccddb9
akashdeep023 <ad3500476@gmail.com>
2024-11-21 02:02:00 +0530

    Update task controller

--- backend/controllers/task.js
+++ backend/controllers/task.js
@@ -5,22 +5,69 @@ const getTask = async (req, res) => {
 	let task = await Task.findById(id);
 	res.status(200).send({ message: "success", data: task });
 };
+
 const getAllTask = async (req, res) => {
-	let backlog = await Task.find({ category: "backlog" });
-	let todo = await Task.find({ category: "to-do" });
-	let inProgress = await Task.find({ category: "in-progress" });
-	let done = await Task.find({ category: "done" });
+	let backlog = await Task.find({
+		$or: [
+			{ $and: [{ userName: req.user._id }, { category: "backlog" }] },
+			{ $and: [{ assign: req.user.email }, { category: "backlog" }] },
+		],
+	}).populate({
+		path: "userName",
+		select: "name",
+	});
+	let todo = await Task.find({
+		$or: [
+			{ $and: [{ userName: req.user._id }, { category: "to-do" }] },
+			{ $and: [{ assign: req.user.email }, { category: "to-do" }] },
+		],
+	}).populate({
+		path: "userName",
+		select: "name",
+	});
+	let inProgress = await Task.find({
+		$or: [
+			{ $and: [{ userName: req.user._id }, { category: "in-progress" }] },
+			{ $and: [{ assign: req.user.email }, { category: "in-progress" }] },
+		],
+	}).populate({
+		path: "userName",
+		select: "name",
+	});
+	let done = await Task.find({
+		$or: [
+			{ $and: [{ userName: req.user._id }, { category: "done" }] },
+			{ $and: [{ assign: req.user.email }, { category: "done" }] },
+		],
+	}).populate({
+		path: "userName",
+		select: "name",
+	});
 	res.status(200).send({
 		message: "success",
 		data: { backlog, todo, inProgress, done },
 	});
 };
+
 const addTask = async (req, res) => {
 	let { title, priority, checklist, dueDate } = req.body;
-	let newTask = new Task({ title, priority, checklist, dueDate });
-	let task = await newTask.save();
+	let userName = req.user._id;
+	let newTask = new Task({
+		title,
+		priority,
+		checklist,
+		dueDate,
+		userName,
+	});
+	let createTask = await newTask.save();
+	console.log(createTask);
+	let task = await Task.findById(createTask._id).populate({
+		path: "userName",
+		select: "name",
+	});
 	res.status(200).send({ message: "success", data: task });
 };
+
 const updateTask = async (req, res) => {
 	let { title, priority, checklist, dueDate } = req.body;
 	const { id } = req.params;
@@ -28,7 +75,10 @@ const updateTask = async (req, res) => {
 		id,
 		{ title, priority, checklist, dueDate },
 		{ new: true }
-	);
+	).populate({
+		path: "userName",
+		select: "name",
+	});
 	res.status(200).send({ message: "success", data: updatedTask });
 };
 const deleteTask = async (req, res) => {
