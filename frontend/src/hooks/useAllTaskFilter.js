981cee3f82114a3ba283574606fc0d4710de79c3
akashdeep023 <ad3500476@gmail.com>
2024-11-22 00:18:42 +0530

    Create useAllTaskFilter hook

---
+++ frontend/src/hooks/useAllTaskFilter.js
@@ -0,0 +1,34 @@
+import getHeader from "../utils/header";
+import {
+	setBacklog,
+	setTodo,
+	setInProgress,
+	setDone,
+} from "../redux/slices/taskSlice";
+import { setLoading } from "../redux/slices/stateSlice";
+import { toast } from "react-toastify";
+
+const useAllTaskFilter = (dispatch, filter) => {
+	dispatch(setLoading(true));
+	fetch(`${import.meta.env.VITE_BACKEND_URL}/api/task/all?filter=${filter}`, {
+		method: "GET",
+		headers: getHeader(),
+	})
+		.then((response) => response.json())
+		.then((json) => {
+			if (json?.message === "success") {
+				dispatch(setBacklog(json.data.backlog));
+				dispatch(setTodo(json.data.todo));
+				dispatch(setInProgress(json.data.inProgress));
+				dispatch(setDone(json.data.done));
+			}
+			dispatch(setLoading(false));
+		})
+		.catch((error) => {
+			console.error("Error:", error);
+			toast.error("Something went wrong");
+			dispatch(setLoading(false));
+		});
+};
+
+export default useAllTaskFilter;
