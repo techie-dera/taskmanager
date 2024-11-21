6685de9e411f3c585866ffbe561981f62a440bc0
akashdeep023 <ad3500476@gmail.com>
2024-11-21 15:27:01 +0530

    Create task UpdateCategory route

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -6,6 +6,7 @@ const {
 	addTask,
 	updateTask,
 	deleteTask,
+	updateCategory,
 } = require("../controllers/task");
 const wrapAsync = require("../middlewares/wrapAsync");
 const { authorization } = require("../middlewares/authorization");
@@ -15,5 +16,6 @@ router.get("/:id", wrapAsync(getTask));
 router.post("/add", authorization, wrapAsync(addTask));
 router.put("/:id", authorization, wrapAsync(updateTask));
 router.delete("/:id", authorization, wrapAsync(deleteTask));
+router.put("/category/:id", authorization, wrapAsync(updateCategory));
 
 module.exports = router;
